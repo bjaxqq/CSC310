9d872fc02ce4823b13f872f298f997705886a588
user <user@user.local>
2025-09-11 15:56:18 -0400

    Updated code

--- Homework 1/bajackson1/assign1.c
+++ Homework 1/bajackson1/assign1.c
@@ -1,20 +1,26 @@
 // assign1.c
 // Author: bajackson1@quinnipiac.edu
 
+// Packages
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Struct to represent a node in a pid list
 typedef struct pid_node {
-    int pid;
-    struct pid_node *next;
-} pid_node;
+    int pid ;
+    struct pid_node * next ;
+} pid_node ;
 
+// Struct to represent a process
 typedef struct pcb {
-    int pid;
-    int ppid;
-    pid_node *children;
-} pcb;
+    int pid ;
+    int ppid ;
+    struct pid_node * children ;
+} pcb ;
 
+// Main function
 int main() {
-    // IMPLEMENT
+    printf("Test message");
     return 0;
 }
